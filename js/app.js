6a8dd06f9f9679c644284c7ede13c6db31e45cf8
ibrahimyolbir <ibrahimyolbirr@gmail.com>
2018-03-21 10:45:53 +0100

    edited products

--- js/app.js
+++ js/app.js
@@ -4,31 +4,111 @@ $(document).ready(function(){
 var cart = [];
 
 var products = [
-    {name : "Samsung", price : "2220 Kr", description : "QLED 55 \"",picture: 'img/samsungtv.png', category: 'elektronik' },
-    {name : "Sony", price : "2220 Kr", description : "QLED 55 \"",picture: 'img/sonytv.png', category: 'elektronik' },
+{
+    name : "Samsung", 
+    price : "2220:-", 
+    description : "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    picture: 'img/samsungtv.png', 
+    category: 'elektronik' 
+},
+{   
+    name : "Sony", 
+    price : "2220:-", 
+    description : "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    picture: 'img/sonytv.png', 
+    category: 'elektronik'
+ },
 
-    {name : "Nike", price : "620 Kr", description : "Skjorta-women",picture: 'img/nikeshoes.png', category: 'klader' },
-    {name : "Adidas", price : "120 Kr", description : "byxa-man",picture: 'img/adidastshirt.png', category: 'klader' },
+{   
+    name : "Nike", 
+    price : "620:-", 
+    description : "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    picture: 'img/nikeshoes.png',    
+    category: 'klader' 
+},
+{
+    name : "Adidas", 
+    price : "120:-", 
+    description : "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    picture: 'img/adidastshirt.png', 
+    category: 'klader' 
+},
     
-    {name : "Lego City", price : "520 Kr", description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",picture: 'img/lego_city.png', category: 'toys' },
-    {name : "Monopoly", price : "320 Kr", description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",picture: 'img/monopoly.png', category: 'toys' },
+{   
+    name : "Lego City", 
+    price : "520:-", 
+    description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+    picture: 'img/lego_city.png', 
+    category: 'toys'
+},
+{   
+    name : "Monopoly", 
+    price : "320:-", 
+    description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+    picture: 'img/monopoly.png', 
+    category: 'toys' 
+},
     
-    {name : "Iphone", price : "8920 Kr", description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",picture: 'img/iphone.png', category: 'phones' },
-    {name : "Asus", price : "5220 Kr", description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",picture: 'img/asus.png', category: 'phones' },
+{   
+    name : "Iphone", 
+    price : "8920:-", 
+    description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+    picture: 'img/iphone.png', 
+    category: 'phones'
+},
+{   
+    name : "Asus", 
+    price : "5220:-", 
+    description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+    picture: 'img/asus.png', 
+    category: 'phones' 
+},
     
-    {name : "Craft Fleece Jacket", price : "2220 Kr", description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",picture: 'img/jacket.png', category: 'sport' },
-    {name : "Salming Speed 3 Shoe", price : "2220 Kr", description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",picture: 'img/shoe2.png', category: 'sport' }
+{   
+    name : "Craft Fleece Jacket", 
+    price : "2220:-", 
+    description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+    picture: 'img/jacket.png', 
+    category: 'sport' 
+},
+{   
+    name : "Salming Speed 3 Shoe", 
+    price : "2220:-", 
+    description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+    picture: 'img/shoe2.png', 
+    category: 'sport'
+}
 ];
 
 var productText = "<ul class=\"product_list\">" ;
 for (var i = 0; i < products.length; i++) {
-    productText += "<li><span class=\"name\">" + products[i].name + "</span> <span class=\"description\">" + products[i].description 
-    + "<img class=\"picture\">" + products[i].picture + "</img>"+"</span> <span class=\"price\">" + products[i].price + "</span>" + "<button id=\"" +i+ "\" class=\"add_item\" type=\"submit\">Välj</button>" + "</li>";
+    productText += 
+        `<li>
+            <div class="item">
+                <div class="item_images">
+                    <img src="${products[i].picture}" alt="">
+                </div>
+                <div class="item_desciraption">
+                    <h5>${products[i].name}</h5>
+                    <p>${products[i].description}</p>
+                </div>
+                <div class="item_button">
+                    <div class="row">
+                         <div class="price_item col-xs-12 col-md-5">
+                            <p>${products[i].price}</p>
+                        </div>
+                    <div class="add_item col-sm-7">
+                        <button id="${i}"  type="submit">Lägg i Varukorg</button>
+                    </div>
+                </div>
+                </div>
+            </div>
+         </li>`;
 }
 
 productText += "</ul>";
 
-$("#product_container").html(productText);
+$(".products").html(productText);
 
 $(".add_item").click(function(){
     var id = $(this).attr("id");
