4529041f2db06abb4e3d64db5c5170459f06bbaa
ibrahimyolbir <ibrahimyolbirr@gmail.com>
2018-03-23 08:17:44 +0100

    site header

--- js/app.js
+++ js/app.js
@@ -101,6 +101,8 @@ $(".add_item").click(function(){
     var product = products[id];
     cart.push(product);
     displayCart();
+    $('span.amount').text(cart.length);
+    $(location).append(item);
 });
 
 function displayCart() {
@@ -158,7 +160,8 @@ function displayCart() {
                   </tr>
                 </tbody>
               </table>
-</div>` }
+</div>` 
+$('span.amount').text(array.length);}
     $(".cart_list").html(cartText);
 }
 $('.shopping_list').on('click', '.delete_item', function (e) {
@@ -169,3 +172,31 @@ $('.shopping_list').on('click', '.delete_item', function (e) {
 
 
 });
+
+
+
+
+// Yukari kaydirma Buton foksiyonu
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 100)    // Sayfa ne kadar aşağı kayarsa buton görünsün. 100 sayısı = Kaydırma çubuğunun piksel konumu. Bu sayı değiştirilebilir.
+        $("#yukari").fadeIn(500);    // Yukarı çık butonu ne kadar hızla ortaya çıksın. 500 milisaniye = 0,5 saniye. Bu sayı değiştirilebilir.
+    else 
+        $("#yukari").fadeOut(500);    // Yukarı çık butonu ne kadar hızla ortadan kaybolsun. 500 milisaniye = 0,5 saniye. Bu sayı değiştirilebilir.
+        
+});
+$(document).ready(function(){
+    $("#yukari").click(function(){    // Yukarı çık butonuna tıklanıldığında aşağıdaki satır çalışır.
+        $("html, body").animate({ scrollTop: "0" }, 500);    // Sayfa ne kadar hızla en yukarı çıksın.
+        // 0 sayısı sayfanın en üstüne çıkılacağını belirtir.
+        // 500 sayısı ne kadar hızla çıkılacağını belirtir. 500 milisaniye = 0,5 saniye. Bu sayı değiştirilebilir.
+        return false;
+    });
+//Kaydirma hizini ayarlama
+    $('.nav_href').click(function(){
+        $('html, body').animate({
+            scrollTop: $( $(this).attr('href') ).offset().top
+        }, 1000);
+        return false;
+    });
+});
+
